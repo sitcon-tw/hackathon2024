23798d7814a97e68e01f36aa4129765359cb2760
SiriusKoan <me@siriuskoan.one>
2024-03-25 11:59:43 +0000

    feat: text for registration

---
+++ src/components/registration/timeline.tsx
@@ -0,0 +1,16 @@
+import { Box } from '@mui/material';
+
+export default function Timeline() {
+    return (
+        <Box>
+            <h2>活動時程</h2>
+            <li>開始報名 4/8</li>
+            <li>報名截止 5/9</li>
+            <li>正取公告 5/13</li>
+            <li>繳費截止 5/20</li>
+            <li>賽前工作坊開始 5/25</li>
+            <li>賽前工作坊結束 7/1</li>
+            <li>SITCON 黑客松 7/6 ~ 7/7</li>
+        </Box>
+    );
+}
