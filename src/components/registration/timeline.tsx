9d9abebb9a6fd6f7962e27efa1a01a45b9f60015
SiriusKoan <me@siriuskoan.one>
2024-03-31 06:30:32 +0000

    feat: add timeline

--- src/components/registration/timeline.tsx
+++ src/components/registration/timeline.tsx
@@ -1,16 +1,17 @@
 import { Box } from '@mui/material';
+import Image from 'next/image';
 
 export default function Timeline() {
     return (
         <Box>
             <h2>活動時程</h2>
-            <li>開始報名 4/8</li>
-            <li>報名截止 5/9</li>
-            <li>正取公告 5/13</li>
-            <li>繳費截止 5/20</li>
-            <li>賽前工作坊開始 5/25</li>
-            <li>賽前工作坊結束 7/1</li>
-            <li>SITCON 黑客松 7/6 ~ 7/7</li>
+            <Image
+                src={`/2024/images/timeline.svg`}
+                alt="timeline"
+                width={0}
+                height={0}
+                style={{ width: '80%', height: 'auto' }}
+            />
         </Box>
     );
 }
