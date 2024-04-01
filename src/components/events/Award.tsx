2832ba552d2e37156493405abbdab58e6c2cd7ab
Ji Kuai <rk427454171@gmail.com>
2024-04-01 20:48:08 +0800

    feat: Events 70% done

---
+++ src/components/events/Award.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import Image from 'next/image';
+import { Typography, Box, Grid } from "@mui/material";
+
+export default function Award() {
+    return (
+        <Box>
+            <Typography variant='h2' fontWeight='bold'>獎勵方法 </Typography>
+            <Typography variant='body1'>
+                各個資源提供企業會在最後評選出兩個優秀團隊予以獎項。
+            </Typography>
+            <Grid container sx={{color: '#B7EFFF'}}>
+                <Grid item xs={3}>
+                    <Grid container flexDirection='row' alignItems='center'>
+                        <Image src='/2024/images/events/medal.svg' alt='svg' width='35' height='35'/>
+                        <Typography variant='h4' fontWeight='bold'>創意獎</Typography>
+                    </Grid>
+                    <Typography variant='h4' fontWeight='bold'>獎金: 4000 元 / 團隊</Typography>
+                </Grid>
+                <Grid item xs={3}></Grid>
+                <Grid item xs={3}>
+                    <Grid container flexDirection='row' alignItems='center'>
+                        <Image src='/2024/images/events/medal.svg' alt='svg' width='35' height='35'/>
+                        <Typography variant='h4' fontWeight='bold'>技術獎</Typography>
+                    </Grid>
+                    <Typography variant='h4' fontWeight='bold'>獎金: 4000 元 / 團隊</Typography>
+                </Grid>
+            </Grid>
+        </Box>
+    )
+}
---
+++ src/components/events/Workshop.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { Typography, Box } from "@mui/material"
+
+export default function Workshop() {
+    return (
+        <Box>
+            <Typography variant='h2' fontWeight='bold'>賽前工作坊</Typography>
+            <Typography variant='body1'>
+                <b>本次競賽合作的企業方會提供賽前工作坊。</b>透過工作坊，參與者能夠更加熟悉的運用企業提供的資源及服務，建立更強的技能基礎，並學習如何將企業提供的資源與服務整合至他們的專案中。
+            </Typography>
+        </Box>
+    )
+}
