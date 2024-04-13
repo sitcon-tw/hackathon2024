192ad07f302c61e143d5b99cebe59ff4c4179ea9
SiriusKoan <me@siriuskoan.one>
2024-04-13 11:34:55 +0000

    feat: registration page mobile and move to (website)

---
+++ src/app/(website)/registration/page.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import Box from '@mui/material/Box';
+import ThemeProvider from '@mui/material/styles/ThemeProvider';
+import createTheme from '@mui/material/styles/createTheme';
+import Typography from '@mui/material/Typography';
+import Timeline from '@/components/registration/timeline';
+import Rule from '@/components/registration/rule';
+import Notice from '@/components/registration/notice';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Noto Sans TC',
+    h1: {
+      fontSize: '48px'
+    },
+    h3: {
+      fontSize: '30px'
+    },
+    body1: {
+      fontSize: '20px'
+    }
+  }
+});
+
+
+export default function About() {
+    return (
+      <ThemeProvider theme={theme}>
+        <main id="registration" style={{ margin: '10px 5vw' }}>
+            <Box style={{ position: 'relative' }}>
+              <Typography variant="h1" style={{ margin: '30px 0' }}>報名資訊</Typography>
+              <Timeline />
+              <Rule />
+              <Notice />
+            </Box>
+        </main>
+      </ThemeProvider>
+    )
+}
--- src/components/registration/timeline.tsx
+++ src/components/registration/timeline.tsx
@@ -1,18 +1,21 @@
 import { Box } from '@mui/material';
+import Typography from '@mui/material/Typography';
 import Image from 'next/image';
+import useMediaQuery from '@mui/material/useMediaQuery';
 import Ellipse from './ellipse';
 
 export default function Timeline() {
+    const isMobile = useMediaQuery('(max-width: 600px)');
     return (
         <Box style={{ position: 'relative' }}>
-            <h2>活動時程</h2>
+            <Typography variant="h3">活動時程</Typography>
             <Ellipse bgColor="#35EB934D" />
             <Image
-                src={`/2024/images/timeline.svg`}
+                src={isMobile ? `/2024/images/timeline-mobile.svg` : `/2024/images/timeline.svg`}
                 alt="timeline"
                 width={0}
                 height={0}
-                style={{ width: '80%', height: 'auto', margin: '10px' }}
+                style={{ width: '80%', height: 'auto', margin: '10px', zIndex: -1 }}
             />
         </Box>
     );
