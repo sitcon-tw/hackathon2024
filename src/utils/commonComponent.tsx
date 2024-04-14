615304560733d868a17460110e3f7661e1cd6aef
Ji Kuai <rk427454171@gmail.com>
2024-04-14 20:47:22 +0800

    Feat: finish schedule. adjust margin between paragraph

--- src/utils/commonComponent.tsx
+++ src/utils/commonComponent.tsx
@@ -13,3 +13,27 @@ export function UnselectableImage({ ...props }: ImageProps) {
     </Box>
   );
 }
+
+interface GradientProps {
+   color: string,
+   transparent?: string,
+   size?: string,
+   children: JSX.Element,
+};
+export function WithGradient<T extends GradientProps>({ children, color, transparent = '66', size = '185px', ...props }: T) {
+  return (
+    <Box>
+        <Box sx={{
+          width: size,
+          height: size,
+          background: `radial-gradient(closest-side, ${color}${transparent}, transparent)`,
+          position: 'absolute',
+          marginTop: '-40px',
+          marginLeft: '10px',
+          zIndex: -1,
+        }} {...props}>
+        </Box>
+        {children}
+    </Box>
+  );
+}
