b746000f92b9c02ba4569f76c39e90805eedd8b4
Ji Kuai <rk427454171@gmail.com>
2024-04-15 21:21:51 +0800

    Fix: add missing content

--- src/components/events/Award.tsx
+++ src/components/events/Award.tsx
@@ -12,13 +12,13 @@ export default function Award() {
                 <Typography variant='h2' fontWeight='bold' marginBottom='56px'>獎勵方法 </Typography>
             </WithGradient>
             <Typography variant='body1' marginBottom='48px'>
-                各個資源提供企業會在最後評選出兩個優秀團隊予以獎項。
+                各個資源提供企業會在最後評選出兩個優秀團隊予以獎項，共 8 組獎項，總獎金 3.2 萬。
             </Typography>
             <Grid container sx={{color: '#B7EFFF'}}>
                 <Grid item xs={3} container gap='20px'>
                     <Grid container flexDirection='row' alignItems='center' gap='15px'>
                         <Image src='/2024/images/events/medal.svg' alt='svg' width='35' height='35'/>
-                        <Typography variant='h4' fontWeight='bold'>創意獎</Typography>
+                        <Typography variant='h4' fontWeight='bold'>創意獎*4</Typography>
                     </Grid>
                     <Typography variant='h4' fontWeight='bold'>獎金: 4000 元 / 團隊</Typography>
                 </Grid>
@@ -26,7 +26,7 @@ export default function Award() {
                 <Grid item xs={3} container gap='20px'>
                     <Grid container flexDirection='row' alignItems='center' gap='15px'>
                         <Image src='/2024/images/events/medal.svg' alt='svg' width='35' height='35'/>
-                        <Typography variant='h4' fontWeight='bold'>技術獎</Typography>
+                        <Typography variant='h4' fontWeight='bold'>技術獎*4</Typography>
                     </Grid>
                     <Typography variant='h4' fontWeight='bold'>獎金: 4000 元 / 團隊</Typography>
                 </Grid>
--- src/components/resources/course.tsx
+++ src/components/resources/course.tsx
@@ -4,7 +4,9 @@ import { Grid, Typography } from "@mui/material";
 import { FaCheck } from "react-icons/fa";
 
 const data = [
-    { name: '零時小學校', url: '#' },
+    { name: '零時小學校', url: 'https://g0v.hackmd.io/@jothon/Sch001courses/https%3A%2F%2Fg0v.hackmd.io%2F%40jothon%2FB1NIQnmXs' },
+    { name: 'Git 入門指南', url: 'https://nulab.com/zh-tw/learn/software-development/git-tutorial/' },
+    { name: 'AWS Educate', url: 'https://aws.amazon.com/tw/education/awseducate/' },
 ];
 
 const buttonText = {
@@ -14,16 +16,13 @@ const buttonText = {
 export default function Course() {
     return (
         <Grid container gap='56px' direction='column'>
-            <Grid item>
-                <Typography id="course" variant='h2' fontWeight='bold'>
-                    線上課程
-                </Typography>
-            </Grid>
-            <Grid item marginLeft='28px'>
+            <Typography id="course" variant='h2' fontWeight='bold'>
+                線上課程
+            </Typography>
+            <Grid item container direction='row' justifyContent='space-around' flexWrap='nowrap'>
                 { data.map(({ name, url }, index) => (
-                        <Grid container key={index} direction='row' alignItems='center'>
-                            <FaCheck size='26px' />
-                            <Typography variant='h4' fontWeight='bold' marginLeft='12px' marginRight='30px'>
+                        <Grid display='inline-flex' item container key={index} direction='row' alignItems='center' gap='10px'>
+                            <Typography variant='h4' fontWeight='bold'>
                                 {name}
                             </Typography>
                             <a className='btn-color' href={url} style={{padding: '5px 15px'}}>
