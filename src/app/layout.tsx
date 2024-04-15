269683134b47912e95042eeafb601802227e90c8
Mirumo <mysteryperson520@gmail.com>
2024-04-16 02:52:12 +0800

    Chore: add ga

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
 import '~/globals.css';
 import { Metadata } from 'next';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 interface RootLayoutProps {
   children: React.ReactNode;
@@ -13,7 +14,18 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
+      <head>
+        <GoogleAnalytics gaId="GTM-NWJD4852" />
+      </head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NWJD4852"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
       </body>
     </html>
