33ee949bd7c13c8eebf847ed6ed55b333cc371a3
Ji Kuai <rk427454171@gmail.com>
2024-04-20 17:12:49 +0800

    feat: add z-index to move picture below

--- src/components/registration/rule.tsx
+++ src/components/registration/rule.tsx
@@ -12,7 +12,14 @@ export default function Rule() {
         alt="registration-rule"
         width={0}
         height={0}
-        style={{ width: '40%', height: 'auto', position: 'absolute', left: '60%', top: '0' }}
+        style={{
+          zIndex: '-1',
+          width: '40%',
+          height: 'auto',
+          position: 'absolute',
+          left: '60%',
+          top: '0'
+        }}
       />
       <Box style={{ position: 'relative' }}>
         <Ellipse bgColor="#2CACD166" />
